b538f2b1b499d51bd5b78f20833127dbcbc14dd1
Thomas McCausland <thomas.mccausland11@gmail.com>
2022-10-17 23:05:10 +0100

    Implement hyperparameter training from tutorial

--- genre-bot/main.py
+++ genre-bot/main.py
@@ -1,5 +1,6 @@
 import os
 import time
+from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,10 +10,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
+from torch.utils.data import random_split
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, l1=1024, l2=512):
         super().__init__()
         self.network = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
@@ -34,153 +39,190 @@ class Net(nn.Module):
             nn.MaxPool2d(2, 2),
 
             nn.Flatten(),
-            nn.Linear(256*4*4, 1024),
+            nn.Linear(256*4*4, l1),
             nn.ReLU(),
-            nn.Linear(1024, 512),
+            nn.Linear(l1, l2),
             nn.ReLU(),
-            nn.Linear(512, 10)
+            nn.Linear(l2, 10)
         )
 
     def forward(self, x):
         return self.network(x)
 
 
-def main():
+def load_data(data_dir="./data"):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform)
+
+    testset = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=True, transform=transform)
+
+    return trainset, testset
+
+
+def train_cifar(config, checkpoint_dir=None, data_dir="./data"):
+
+    start = time.time()
+
+    net = Net(config["l1"], config["l2"])
+
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+        if torch.cuda.device_count() > 1:
+            net = nn.parallel.DataParallel(net)
+    net.to(device)
     # Assuming that we are on a CUDA machine, this should print a CUDA device:
     print(f"Using {device} device")
 
-    # Load and normalise CIFAR10
-
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    batch_size = 4
-
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-    print("Done.")
-
-    # functions to show an image
-    def imshow(img):
-        img = img / 2 + 0.5     # unnormalize
-        npimg = img.numpy()
-        plt.imshow(np.transpose(npimg, (1, 2, 0)))
-        plt.show()
-
-    """
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
-
-    imshow(torchvision.utils.make_grid(images))
-    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
-    """
-
-    # Define a Convolutional Neural Network
-    net = Net()
-    net = net.to(device)
-    print(net)
-
-    # Define a Loss function and optimiser
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=config["lr"], momentum=0.9)
 
-    PATH = './cifar_net.pth'
+    if checkpoint_dir:
+        model_state, optimizer_state = torch.load(
+            os.path.join(checkpoint_dir, "checkpoint"))
+        net.load_state_dict(model_state)
+        optimizer.load_state_dict(optimizer_state)
 
-    # Train the network if one is not already saved
+    trainset, testset = load_data(data_dir)
 
-    epochs = 4
+    test_abs = int(len(trainset) * 0.8)
+    train_subset, validation_subset = random_split(trainset, [test_abs, len(trainset) - test_abs])
 
-    if not os.path.isfile(PATH):
-        start = time.time()
-        for epoch in range(epochs):  # loop over the dataset multiple times
-            running_loss = 0.0
-            for i, data in enumerate(trainloader, 0):
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data[0].to(device), data[1].to(device)
+    trainloader = torch.utils.data.DataLoader(
+        train_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=8)
+    validationloader = torch.utils.data.DataLoader(
+        validation_subset,
+        batch_size=int(config["batch_size"]),
+        shuffle=True,
+        num_workers=8)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+    for epoch in range(10):  # loop over the dataset multiple times
+        running_loss = 0.0
+        epoch_steps = 0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
 
-                # forward + backward + optimize
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print some statistics
+            running_loss += loss.item()
+            epoch_steps += 1
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / epoch_steps))
+                running_loss = 0.0
+
+        # Validation loss
+        val_loss = 0.0
+        val_steps = 0
+        total = 0
+        correct = 0
+        for i, data in enumerate(validationloader, 0):
+            with torch.no_grad():
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+
                 outputs = net(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
                 loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
+                val_loss += loss.cpu().numpy()
+                val_steps += 1
 
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                    running_loss = 0.0
-        end = time.time()
-        print(f'Training took {end - start} seconds')
-        # torch.save(net.state_dict(), PATH)
-    else:
-        net.load_state_dict(torch.load(PATH))
+        with tune.checkpoint_dir(epoch) as checkpoint_dir:
+            path = os.path.join(checkpoint_dir, "checkpoint")
+            torch.save((net.state_dict(), optimizer.state_dict()), path)
 
-    # Test the network on the test data
-    """
-    dataiter = iter(testloader)
-    images, labels = dataiter.next()
+        tune.report(loss=(val_loss / val_steps), accuracy=correct / total)
 
-    imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+    end = time.time()
+    print(f'Training took {end - start} seconds')
 
-    outputs = net(images)
 
-    _, predicted = torch.max(outputs, 1)
+def test_accuracy(net, device="cpu"):
+    trainset, testset = load_data()
 
-    print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}'for j in range(4)))
-    """
-    start = time.time()
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
     correct = 0
     total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in testloader:
-            images, labels = data[0].to(device), data[1].to(device)
-            # calculate outputs by running images through the network
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
             outputs = net(images)
-            # the class with the highest energy is what we choose as prediction
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+    return correct / total
 
-    # prepare to count predictions for each class
-    correct_pred = {classname: 0 for classname in classes}
-    total_pred = {classname: 0 for classname in classes}
 
-    # again no gradients needed
-    with torch.no_grad():
-        for data in testloader:
-            images, labels = data[0].to(device), data[1].to(device)
-            outputs = net(images)
-            _, predictions = torch.max(outputs, 1)
-            # collect the correct predictions for each class
-            for label, prediction in zip(labels, predictions):
-                if label == prediction:
-                    correct_pred[classes[label]] += 1
-                total_pred[classes[label]] += 1
+def main(num_samples=10, max_num_epochs=10, gpus_per_trial=1):
+    data_dir = os.path.abspath("./data")
+    load_data(data_dir)
 
-    # print accuracy for each class
-    for classname, correct_count in correct_pred.items():
-        accuracy = 100 * float(correct_count) / total_pred[classname]
-        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+    config = {
+        "l1": tune.sample_from(lambda _: 2 ** np.random.randint(6, 11)),
+        "l2": tune.sample_from(lambda _: 2 ** np.random.randint(6, 11)),
+        "lr": tune.loguniform(1e-4, 1e-1),
+        "batch_size": tune.choice([2, 4, 8, 16])
+    }
 
-    end = time.time()
-    print(f'Testing took {end - start} seconds')
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=max_num_epochs,
+        grace_period=1,
+        reduction_factor=2)
+
+    reporter = CLIReporter(
+        # parameter_columns=["l1", "l2", "lr", "batch_size"],
+        metric_columns=["loss", "accuracy", "training_iteration"])
+
+    result = tune.run(
+        partial(train_cifar, data_dir=data_dir),
+        resources_per_trial={"cpu": 2, "gpu": gpus_per_trial},
+        config=config,
+        num_samples=num_samples,
+        scheduler=scheduler,
+        progress_reporter=reporter)
+
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
+    print("Best trial final validation accuracy: {}".format(best_trial.last_result["accuracy"]))
+
+    best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+        if gpus_per_trial > 1:
+            best_trained_model = nn.parallel.DataParallel(best_trained_model)
+    best_trained_model.to(device)
+
+    best_checkpoint_dir = best_trial.checkpoint.value
+    model_state, optimizer_state = torch.load(os.path.join(best_checkpoint_dir, "checkpoint"))
+    best_trained_model.load_state_dict(model_state)
+
+    test_acc = test_accuracy(best_trained_model, device)
+    print("Best trial test set accuracy: {}".format(test_acc))
 
 
 if __name__ == "__main__":
-    main()
+    # You can change the number of GPUs per trial here:
+    main(num_samples=15, max_num_epochs=10, gpus_per_trial=1)
