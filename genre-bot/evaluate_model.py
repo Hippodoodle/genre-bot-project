a4b1be4d45dde59a5e27e27414fe6af93dc34e3f
Thomas McCausland <thomas.mccausland11@gmail.com>
2023-02-02 21:09:54 +0000

    Update evaluate_model.py
    Finished script to load checkpoints, created method to calculate
    f1_score. Additionally imported sklearn f1_score function.

--- genre-bot/evaluate_model.py
+++ genre-bot/evaluate_model.py
@@ -1,21 +1,42 @@
 import json
 import os
-import time
-from functools import partial
 from pathlib import Path
 
-import numpy as np
 import torch
-import torch.nn as nn
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-from main import Net, test_accuracy
-from ray import tune
-from ray.tune import CLIReporter, ExperimentAnalysis
-from ray.tune.experiment import Trial
-from ray.tune.schedulers import ASHAScheduler
-from torch.utils.data import DataLoader, random_split
+from sklearn.metrics import f1_score
+from torch.utils.data import DataLoader
+from train_model import Net, load_data, test_accuracy
+
+
+@torch.no_grad()
+def get_f1_score(net, data_dir: str | Path, device: str = "cpu"):
+    trainset, testset = load_data(data_dir)
+
+    testloader = DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
+
+    score = 0
+    true_positive = 0
+    false_positive = 0
+    true_negative = 0
+    false_negative = 0
+    list1 = []
+    list2 = []
+    for inputs, labels in testloader:
+        inputs: torch.Tensor = inputs.to(device)
+        labels: torch.Tensor = labels.to(device)
+        outputs = net(inputs)
+        predicted: torch.Tensor
+        _, predicted = torch.max(outputs.data, 1)
+        list1.append(labels.tolist()[0])
+        list2.append(predicted.tolist()[0])
+        actual_value = labels.tolist()[0]
+        predicted_value = predicted.tolist()[0]
+
+    score = f1_score(labels.cpu().data, predicted.cpu(), zero_division=0)  # type: ignore
+    score = f1_score(list1, list2, zero_division=0)  # type: ignore
+
+    return score
 
 
 def main():
@@ -24,29 +45,34 @@ def main():
     RESULTS_DIR = "./results/experiment_5_genres/"
     CLASSES = 2
 
-    for experiment_dir in os.listdir(RESULTS_DIR):
-        experiment_dir = os.path.join(RESULTS_DIR, experiment_dir)
+    for experiment_basename in os.listdir(RESULTS_DIR):
+
+        # Get data directory
+        data_dir = os.path.join(DATA_DIR, experiment_basename)
+
+        # Get experiment directory
+        experiment_dir = os.path.join(RESULTS_DIR, experiment_basename)
         experiment_dir = max([os.path.join(experiment_dir, checkpoint_file) for checkpoint_file in os.listdir(experiment_dir)], key=os.path.getctime)
 
-        checkpoint_dir = os.path.join(experiment_dir,
-                                      [x for x in os.listdir(experiment_dir) if os.path.isdir(os.path.join(experiment_dir, x))][0])
+        # Get checkpoint directory
+        checkpoint_dir = os.path.join(experiment_dir, [x for x in os.listdir(experiment_dir) if os.path.isdir(os.path.join(experiment_dir, x))][0])
 
         # Load checkpoint config
         with open(os.path.join(checkpoint_dir, "params.json")) as json_file:
             config = json.load(json_file)
 
         # Get latest checkpoint file
-        latest_checkpoint = os.path.join(checkpoint_dir,
-                                         max([x for x in os.listdir(checkpoint_dir) if os.path.isdir(os.path.join(checkpoint_dir, x))]),
-                                         "checkpoint")
+        latest_checkpoint_path = os.path.join(checkpoint_dir,
+                                              max([x for x in os.listdir(checkpoint_dir) if os.path.isdir(os.path.join(checkpoint_dir, x))]),
+                                              "checkpoint")
 
         # Initialise nn and optimiser
         model = Net(config["l1"], config["l2"], CLASSES)
         optimiser = optim.SGD(model.parameters(), lr=config["lr"], momentum=0.9)
 
-        checkpoint = torch.load(latest_checkpoint)
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimiser.load_state_dict(checkpoint['optimizer_state_dict'])
+        model_state, optimizer_state = torch.load(latest_checkpoint_path)
+        model.load_state_dict(model_state)
+        optimiser.load_state_dict(optimizer_state)
 
         model.eval()
 
@@ -55,25 +81,11 @@ def main():
             device = "cuda:0"
         model.to(device)
 
-        """
-        best_trial: Trial = result.get_best_trial("loss", "min", "last")  # type: ignore
-        print("Best trial config: {}".format(best_trial.config))
-        print("Best trial final validation loss: {}".format(best_trial.last_result["loss"]))
-        print("Best trial final validation accuracy: {}".format(best_trial.last_result["accuracy"]))
+        test_acc = test_accuracy(model, device, data_dir)
+        print(f"Accuracy for {experiment_basename} experiment: {test_acc}")
 
-        best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
-        device = "cpu"
-        if torch.cuda.is_available():
-            device = "cuda:0"
-        best_trained_model.to(device)
-
-        best_checkpoint_dir: str | Path = best_trial.checkpoint.dir_or_data  # type: ignore
-        model_state, optimizer_state = torch.load(os.path.join(best_checkpoint_dir, "checkpoint"))
-        best_trained_model.load_state_dict(model_state)
-
-        test_acc = test_accuracy(best_trained_model, device)
-        print("Best trial test set accuracy: {}".format(test_acc))
-        """
+        f1_score = get_f1_score(model, data_dir, device)
+        print(f"F1 score for {experiment_basename} experiment: {f1_score}")
 
 
 if __name__ == "__main__":
